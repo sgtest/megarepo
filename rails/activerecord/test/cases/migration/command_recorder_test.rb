rails/rails

commit c278a2c5e109204ec8a47fcbfdfc327aad7996ce

record unsupported methods in CommandRecorder instead of letting the unsupported methods go through to the underlying db causing errors like duplicate columns to occur when rolling back migrations

activerecord/lib/active_record/migration/command_recorder.rb
@@ -93,11 +93,11 @@ module ActiveRecord
         [:remove_timestamps, args]
       end
 
-      # Forwards any missing method call to the \target.
+      # Record all the methods called in the +change+ method of a migration.
+      # This will ensure that IrreversibleMigration is raised when the corresponding
+      # invert_method does not exist while the migration is rolled back.
       def method_missing(method, *args, &block)
-        @delegate.send(method, *args, &block)
-      rescue NoMethodError => e
-        raise e, e.message.sub(/ for #<.*$/, " via proxy for #{@delegate}")
+        record(method, args)
       end
 
     end

activerecord/test/cases/migration/command_recorder_test.rb
@@ -14,9 +14,11 @@ module ActiveRecord
         assert recorder.respond_to?(:america)
       end
 
-      def test_send_calls_super
-        assert_raises(NoMethodError) do
-          @recorder.send(:non_existing_method, :horses)
+      def test_non_existing_method_records_and_raises_on_inversion
+        @recorder.send(:non_existing_method, :horses)
+        assert_equal 1, @recorder.commands.length
+        assert_raises(ActiveRecord::IrreversibleMigration) do
+          @recorder.inverse
         end
       end
 

sgtest/megarepo

commit e5ec2f85d2678546754bdf536f653f317bb82885

rails: CommandRecorder should delegate in method_missing where possible. Fixes some tests in migration_test.rb under mysql. The problem was introduced in c278a2c5e109204ec8a47fcbfdfc327aad7996ce.
Commit: 4d256bc6b1e6ae62c78d0ff3ca17480a53e00436

rails/activerecord/lib/active_record/migration/command_recorder.rb
@@ -97,7 +97,11 @@ module ActiveRecord
       # This will ensure that IrreversibleMigration is raised when the corresponding
       # invert_method does not exist while the migration is rolled back.
       def method_missing(method, *args, &block)
-        record(method, args)
+        if delegate.respond_to?(method)
+          delegate.send(method, *args, &block)
+        else
+          record(method, args)
+        end
       end
 
     end

rails/activerecord/test/cases/migration/command_recorder_test.rb
@@ -31,7 +31,12 @@ module ActiveRecord
         assert_equal [[:create_table, [:horses]]], recorder.commands
       end
 
-      def test_unknown_commands_raise_exception
+      def test_unknown_commands_delegate
+        recorder = CommandRecorder.new(stub(:foo => 'bar'))
+        assert_equal 'bar', recorder.foo
+      end
+
+      def test_unknown_commands_raise_exception_if_they_cannot_delegate
         @recorder.record :execute, ['some sql']
         assert_raises(ActiveRecord::IrreversibleMigration) do
           @recorder.inverse