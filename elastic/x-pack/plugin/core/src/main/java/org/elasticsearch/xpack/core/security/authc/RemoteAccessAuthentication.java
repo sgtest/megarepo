sgtest/megarepo

commit 57d3e8ccd6de4a659703680841c04913ddde2223

elastic: Consolidate use of `RemoteAccessHeaders`  (#93662)
Commit: 7d88d13312e5be993f202a013951a252f5cb6a89

elastic/x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/RemoteAccessAuthentication.java
@@ -56,7 +56,11 @@ public final class RemoteAccessAuthentication {
     }
 
     public static RemoteAccessAuthentication readFromContext(final ThreadContext ctx) throws IOException {
-        return decode(ctx.getHeader(REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY));
+        final String header = ctx.getHeader(REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY);
+        if (header == null) {
+            throw new IllegalArgumentException("remote access header [" + REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY + "] is required");
+        }
+        return decode(header);
     }
 
     public Authentication getAuthentication() {
@@ -211,6 +215,24 @@ public final class RemoteAccessAuthentication {
         @Override
         public String toString() {
             return "RoleDescriptorsBytes{" + "rawBytes=" + rawBytes + '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            if (false == super.equals(o)) return false;
+
+            RoleDescriptorsBytes that = (RoleDescriptorsBytes) o;
+
+            return Objects.equals(rawBytes, that.rawBytes);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = super.hashCode();
+            result = 31 * result + (rawBytes != null ? rawBytes.hashCode() : 0);
+            return result;
         }
     }
 }

elastic/x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authc/RemoteAccessAuthenticationTests.java
@@ -23,6 +23,7 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static org.elasticsearch.xpack.core.security.authc.RemoteAccessAuthentication.REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY;
 import static org.elasticsearch.xpack.core.security.authz.RoleDescriptorTests.randomUniquelyNamedRoleDescriptors;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -56,6 +57,14 @@ public class RemoteAccessAuthenticationTests extends ESTestCase {
         final Set<RoleDescriptor> actualRoleDescriptors = new RemoteAccessAuthentication.RoleDescriptorsBytes(BytesReference.bytes(builder))
             .toRoleDescriptors();
         assertThat(actualRoleDescriptors, equalTo(expectedRoleDescriptors));
+    }
+
+    public void testThrowsOnMissingEntry() {
+        var actual = expectThrows(
+            IllegalArgumentException.class,
+            () -> RemoteAccessAuthentication.readFromContext(new ThreadContext(Settings.EMPTY))
+        );
+        assertThat(actual.getMessage(), equalTo("remote access header [" + REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY + "] is required"));
     }
 
     private RoleDescriptorsIntersection randomRoleDescriptorsIntersection() {

elastic/x-pack/plugin/security/qa/multi-cluster/src/javaRestTest/java/org/elasticsearch/xpack/remotecluster/RemoteClusterSecurityRestIT.java
@@ -183,6 +183,7 @@ public class RemoteClusterSecurityRestIT extends AbstractRemoteClusterSecurityTe
         return client().performRequest(request);
     }
 
+    // TODO centralize common usage of this across all tests
     private static String randomEncodedApiKey() {
         return Base64.getEncoder().encodeToString((UUIDs.base64UUID() + ":" + UUIDs.base64UUID()).getBytes(StandardCharsets.UTF_8));
     }

elastic/x-pack/plugin/security/qa/security-trial/src/javaRestTest/java/org/elasticsearch/xpack/security/remoteaccess/RemoteAccessHeadersForCcsRestIT.java
@@ -53,8 +53,8 @@ import org.elasticsearch.xpack.core.security.authz.RoleDescriptorsIntersection;
 import org.elasticsearch.xpack.core.security.user.SystemUser;
 import org.elasticsearch.xpack.core.security.user.User;
 import org.elasticsearch.xpack.security.SecurityOnTrialLicenseRestTestCase;
+import org.elasticsearch.xpack.security.authc.RemoteAccessHeaders;
 import org.elasticsearch.xpack.security.authz.RBACEngine;
-import org.elasticsearch.xpack.security.transport.SecurityServerTransportInterceptor;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -393,9 +393,9 @@ public class RemoteAccessHeadersForCcsRestIT extends SecurityOnTrialLicenseRestT
     }
 
     private void assertContainsRemoteClusterCredential(String clusterCredential, CapturedActionWithHeaders actual) {
-        assertThat(actual.headers(), hasKey(SecurityServerTransportInterceptor.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY));
+        assertThat(actual.headers(), hasKey(RemoteAccessHeaders.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY));
         assertThat(
-            actual.headers().get(SecurityServerTransportInterceptor.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY),
+            actual.headers().get(RemoteAccessHeaders.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY),
             equalTo("ApiKey " + clusterCredential)
         );
     }
@@ -480,7 +480,7 @@ public class RemoteAccessHeadersForCcsRestIT extends SecurityOnTrialLicenseRestT
             actualHeaders.keySet(),
             containsInAnyOrder(
                 RemoteAccessAuthentication.REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY,
-                SecurityServerTransportInterceptor.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY
+                RemoteAccessHeaders.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY
             )
         );
     }

elastic/x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ApiKeyService.java
@@ -980,12 +980,15 @@ public class ApiKeyService {
         return getCredentialsFromHeader(threadContext.getHeader("Authorization"));
     }
 
-    ApiKeyCredentials getCredentialsFromHeader(final String header) {
-        assert isEnabled() : "API keys must be enabled";
+    static ApiKeyCredentials getCredentialsFromHeader(final String header) {
         return parseApiKey(Authenticator.extractCredentialFromHeaderValue(header, "ApiKey"));
     }
 
-    private ApiKeyCredentials parseApiKey(SecureString apiKeyString) {
+    public static String withApiKeyPrefix(final String encodedApiKey) {
+        return "ApiKey " + encodedApiKey;
+    }
+
+    private static ApiKeyCredentials parseApiKey(SecureString apiKeyString) {
         if (apiKeyString != null) {
             final byte[] decodedApiKeyCredBytes = Base64.getDecoder().decode(CharArrays.toUtf8Bytes(apiKeyString.getChars()));
             char[] apiKeyCredChars = null;
@@ -1087,6 +1090,7 @@ public class ApiKeyService {
         public void clearCredentials() {
             close();
         }
+
     }
 
     private static class ApiKeyLoggingDeprecationHandler implements DeprecationHandler {

elastic/x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/RemoteAccessAuthenticationService.java
@@ -34,7 +34,7 @@ import java.util.function.Supplier;
 
 import static org.elasticsearch.core.Strings.format;
 import static org.elasticsearch.xpack.core.security.authc.RemoteAccessAuthentication.REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY;
-import static org.elasticsearch.xpack.security.transport.SecurityServerTransportInterceptor.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY;
+import static org.elasticsearch.xpack.security.authc.RemoteAccessHeaders.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY;
 
 public class RemoteAccessAuthenticationService {
 
@@ -87,7 +87,8 @@ public class RemoteAccessAuthenticationService {
 
         final RemoteAccessHeaders remoteAccessHeaders;
         try {
-            remoteAccessHeaders = extractRemoteAccessHeaders(threadContext);
+            apiKeyService.ensureEnabled();
+            remoteAccessHeaders = RemoteAccessHeaders.readFromContext(threadContext);
         } catch (Exception ex) {
             withRequestProcessingFailure(authcContext, ex, listener);
             return;
@@ -104,7 +105,7 @@ public class RemoteAccessAuthenticationService {
             )
         ) {
             final Supplier<ThreadContext.StoredContext> storedContextSupplier = threadContext.newRestorableContext(false);
-            authcContext.addAuthenticationToken(remoteAccessHeaders.clusterCredential());
+            authcContext.addAuthenticationToken(remoteAccessHeaders.clusterCredentials());
             authenticationService.authenticate(
                 authcContext,
                 new ContextPreservingActionListener<>(storedContextSupplier, ActionListener.wrap(authentication -> {
@@ -122,7 +123,7 @@ public class RemoteAccessAuthenticationService {
         }
     }
 
-    private static RemoteAccessAuthentication maybeRewriteForSystemUser(RemoteAccessAuthentication remoteAccessAuthentication)
+    private static RemoteAccessAuthentication maybeRewriteForSystemUser(final RemoteAccessAuthentication remoteAccessAuthentication)
         throws IOException {
         final Subject receivedEffectiveSubject = remoteAccessAuthentication.getAuthentication().getEffectiveSubject();
         final User user = receivedEffectiveSubject.getUser();
@@ -146,29 +147,6 @@ public class RemoteAccessAuthenticationService {
 
     public AuthenticationService getAuthenticationService() {
         return authenticationService;
-    }
-
-    private record RemoteAccessHeaders(
-        ApiKeyService.ApiKeyCredentials clusterCredential,
-        RemoteAccessAuthentication remoteAccessAuthentication
-    ) {}
-
-    private RemoteAccessHeaders extractRemoteAccessHeaders(final ThreadContext threadContext) throws IOException {
-        apiKeyService.ensureEnabled();
-        final String clusterCredentialHeader = threadContext.getHeader(REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY);
-        if (clusterCredentialHeader == null) {
-            throw new IllegalArgumentException("remote access header [" + REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY + "] is required");
-        }
-        final ApiKeyService.ApiKeyCredentials apiKeyCredential = apiKeyService.getCredentialsFromHeader(clusterCredentialHeader);
-        if (apiKeyCredential == null) {
-            throw new IllegalArgumentException(
-                "remote access header [" + REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY + "] value must be a valid API key credential"
-            );
-        }
-        if (threadContext.getHeader(REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY) == null) {
-            throw new IllegalArgumentException("remote access header [" + REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY + "] is required");
-        }
-        return new RemoteAccessHeaders(apiKeyCredential, RemoteAccessAuthentication.readFromContext(threadContext));
     }
 
     private void validate(final RemoteAccessAuthentication remoteAccessAuthentication) {

elastic/x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/RemoteAccessHeaders.java
@@ -1,0 +1,76 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.security.authc;
+
+import org.elasticsearch.common.util.concurrent.ThreadContext;
+import org.elasticsearch.xpack.core.security.authc.RemoteAccessAuthentication;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public final class RemoteAccessHeaders {
+
+    public static final String REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY = "_remote_access_cluster_credential";
+    private final String clusterCredentialsHeader;
+    private final RemoteAccessAuthentication remoteAccessAuthentication;
+
+    public RemoteAccessHeaders(String clusterCredentialsHeader, RemoteAccessAuthentication remoteAccessAuthentication) {
+        assert clusterCredentialsHeader.startsWith("ApiKey ") : "cluster credentials headers must start with [ApiKey ]";
+        this.clusterCredentialsHeader = clusterCredentialsHeader;
+        this.remoteAccessAuthentication = remoteAccessAuthentication;
+    }
+
+    public void writeToContext(final ThreadContext ctx) throws IOException {
+        ctx.putHeader(REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY, clusterCredentialsHeader);
+        remoteAccessAuthentication.writeToContext(ctx);
+    }
+
+    public static RemoteAccessHeaders readFromContext(final ThreadContext ctx) throws IOException {
+        final String clusterCredentialsHeader = ctx.getHeader(REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY);
+        if (clusterCredentialsHeader == null) {
+            throw new IllegalArgumentException("remote access header [" + REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY + "] is required");
+        }
+        // Invoke parsing logic to validate that the header decodes to a valid API key credential
+        // Call `close` since the returned value is an auto-closable
+        parseClusterCredentialsHeader(clusterCredentialsHeader).close();
+        return new RemoteAccessHeaders(clusterCredentialsHeader, RemoteAccessAuthentication.readFromContext(ctx));
+    }
+
+    public ApiKeyService.ApiKeyCredentials clusterCredentials() {
+        return parseClusterCredentialsHeader(clusterCredentialsHeader);
+    }
+
+    private static ApiKeyService.ApiKeyCredentials parseClusterCredentialsHeader(final String header) {
+        try {
+            return Objects.requireNonNull(ApiKeyService.getCredentialsFromHeader(header));
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(
+                "remote access header [" + REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY + "] value must be a valid API key credential",
+                ex
+            );
+        }
+    }
+
+    public RemoteAccessAuthentication remoteAccessAuthentication() {
+        return remoteAccessAuthentication;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (RemoteAccessHeaders) obj;
+        return Objects.equals(this.clusterCredentialsHeader, that.clusterCredentialsHeader)
+            && Objects.equals(this.remoteAccessAuthentication, that.remoteAccessAuthentication);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clusterCredentialsHeader, remoteAccessAuthentication);
+    }
+}

elastic/x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/transport/RemoteAccessServerTransportFilter.java
@@ -21,7 +21,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.elasticsearch.xpack.core.security.authc.RemoteAccessAuthentication.REMOTE_ACCESS_AUTHENTICATION_HEADER_KEY;
-import static org.elasticsearch.xpack.security.transport.SecurityServerTransportInterceptor.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY;
+import static org.elasticsearch.xpack.security.authc.RemoteAccessHeaders.REMOTE_ACCESS_CLUSTER_CREDENTIAL_HEADER_KEY;
 
 final class RemoteAccessServerTransportFilter extends ServerTransportFilter {
     // pkg-private for testing

commit 16fc6b9931aa3e387aac98a6d12c4a20c4fa4861

elastic: Fix merge issue introduced in #93662
Commit: f6c05fb075537e6adc7f1db5b433a1f7c59bf5b4

elastic/x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/RemoteAccessAuthentication.java
@@ -216,23 +216,5 @@ public final class RemoteAccessAuthentication {
         public String toString() {
             return "RoleDescriptorsBytes{" + "rawBytes=" + rawBytes + '}';
         }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            if (false == super.equals(o)) return false;
-
-            RoleDescriptorsBytes that = (RoleDescriptorsBytes) o;
-
-            return Objects.equals(rawBytes, that.rawBytes);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = super.hashCode();
-            result = 31 * result + (rawBytes != null ? rawBytes.hashCode() : 0);
-            return result;
-        }
     }
 }